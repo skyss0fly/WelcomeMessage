186d1123cc4ae92d70cc579b98d24c30b8b3e942
Astro-PM <129596573+Astro-PM@users.noreply.github.com>
2023-04-09 11:03:20 +0530

    Update Main.php

--- src/skyss0fly/Main.php
+++ src/skyss0fly/Main.php
@@ -3,12 +3,13 @@
 namespace skyss0fly\WelcomeMessage;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 
 class Main extends PluginBase implements Listener {
 
-    public function onEnable() {
+    public function onEnable(): void {
         $this->getLogger()->info("Welcome Message plugin has been enabled");
         $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
