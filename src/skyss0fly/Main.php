d319d605db07bae56e101e829cc1d6f70a332678
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-09 13:45:26 +1000

    Update Main.php

--- src/skyss0fly/Main.php
+++ src/skyss0fly/Main.php
@@ -8,6 +8,6 @@ public function onEnable() {
 this->getLogger()info()->(TEXTFORMAT::MINECOIN_GOLD . "Welcome Message Has Loaded:) ");
 public function on Join() {
 $this->getServer(->broadcastMessage("Welcome to" . $config this-> new(Servername) "!";
-$this->getLogger0->info("Player has joined.");
+$this->getLogger()info()->("Player has joined.");
 }
 }
