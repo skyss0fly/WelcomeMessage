95826e869b1203e300b50b75ddd1a01a7bca5f57
Inaayat <125380942+Inaay@users.noreply.github.com>
2023-04-09 06:57:56 -0700

    Update Main.php

--- src/skyss0fly/welcomemessage/Main.php
+++ src/skyss0fly/welcomemessage/Main.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace skyss0fly\welcomemessage;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->getLogger()->info("Welcome Message plugin has been enabled");
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $prefix = $this->getConfig()->get("Prefix");
+        $message = $this->getConfig()->get("Message");
+        $message = str_replace("{player}", $player->getName(), $message);
+        $prefix = str_replace("&", "§", $prefix);
+        $message = str_replace("&", "§", $message);
+        $message = $prefix . ": " . $message;
+        $this->getServer()->broadcastMessage($message);
+    }
+}
