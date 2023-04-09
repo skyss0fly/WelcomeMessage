55da4e3f03a292a87a9343b2e58000e3d74d1168
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-09 13:56:06 +1000

    Update Main.php

--- src/skyss0fly/Main.php
+++ src/skyss0fly/Main.php
@@ -4,12 +4,12 @@ use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
-public function onEnable() {
+public function onEnable() ::void {
 
-$this->getLogger()info()->("Welcome Message Has Loaded ");
+$this->getLogger()info->()("Welcome Message Has Loaded ");
 }
 }
-public function PlayerJoin() {
+public function PlayerJoin() ::void {
 $this->getServer(->broadcastMessage("Welcome to" . $config this-> new(Servername) "!";
 $this->getLogger()info()->("Player has joined.");
 }
