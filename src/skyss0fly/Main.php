c2392a1e6855e00f0968d0845cb050aec96cfea3
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-09 13:52:18 +1000

    Update Main.php

--- src/skyss0fly/Main.php
+++ src/skyss0fly/Main.php
@@ -7,8 +7,9 @@ class Main extends PluginBase {
 public function onEnable() {
 
 $this->getLogger()info()->("Welcome Message Has Loaded ");
+}
+}
 public function PlayerJoin() {
 $this->getServer(->broadcastMessage("Welcome to" . $config this-> new(Servername) "!";
 $this->getLogger()info()->("Player has joined.");
-}
 }
