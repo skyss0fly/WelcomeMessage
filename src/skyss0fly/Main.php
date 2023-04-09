a6b718d182798c912d2b7c7dab47eb5380bbb995
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-09 15:19:43 +1000

    Update Main.php

--- src/skyss0fly/Main.php
+++ src/skyss0fly/Main.php
@@ -20,6 +20,6 @@ $this->getConfig()->get("Prefix")
 
 public function PlayerJoin() {
 $this->getLogger()->info("player has joined!");
-$this->getServer()->broadcastMessage($Prefix . ": Welcome to" . $ServerName);
+$this->getServer()->broadcastMessage($Prefix . ": Welcome to " . $ServerName);
 }
 }
