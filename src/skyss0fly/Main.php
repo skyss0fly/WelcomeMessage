fb34233fe0c74aa0f34d205be066a6c0cc8d4bf8
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-09 13:33:54 +1000

    Update Main.php

--- src/skyss0fly/Main.php
+++ src/skyss0fly/Main.php
@@ -1,12 +1,13 @@
 <?php
 namespace skyss0fly\WelcomeMessage;
 use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
 
-
-
+class Main extends PluginBass {
 public function onEnable() {
 this->getLogger()info->(TEXTFORMAT::MINECOIN_GOLD . "Welcome Message Has Loaded:) ");
 public function on Join() {
 $this->getServer(->broadcastMessage("Welcome to" . $config this-> new(Servername) "!";
 $this->getLogger0->info("Player has joined.");
+}
 }
