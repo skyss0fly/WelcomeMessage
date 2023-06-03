90c80ee6169441cbf84e41d7cb4042f465d0ef20
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-03 17:53:13 +1000

    Update Main.php

--- src/skyss0fly/welcomemessage/Main.php
+++ src/skyss0fly/welcomemessage/Main.php
@@ -18,17 +18,23 @@ class Main extends PluginBase implements Listener {
         $servername = $this->getConfig()->get("ServerName");
         $prefix = $this->getConfig()->get("Prefix");
         $message = $this->getConfig()->get("Message");
-        $broadcasttoserver = $this->getConfig()->get("BroadcastToServer");
+        $messageoptions = $this->getConfig()->get("messageoptions");
         $message = str_replace("{player}", $player->getName(), $message);
         $prefix = str_replace("&", "§", $prefix);
         $servername = str_replace("&", "§", $servername);
         $message = str_replace("&", "§", $message);
         $message = $prefix . ": " . $message . $servername;
-if ($broadcasttoserver === true) {
+if ($messageoptions === "broadcast") {
+        $this->getServer()->broadcastMessage($message);
+}  if ($messageoptions === "whisper") {
         $this->getServer()->broadcastMessage($message);
 }
-else {
-    $player->sendMessage($message);
-}
+ if ($messageoptions === "whisper") {
+         $player->sendMessage($message);
+ }
+       if ($messageoptions === "both") {
+$player->sendMessage($message);
+           $this->getServer()->broadcastMessage($message);
+       }
     }
 }
