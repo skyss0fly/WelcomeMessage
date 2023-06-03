eb893c397b29883956b3b7c52d4a7727d67f43bc
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-03 21:48:01 +1000

    Update Main.php

--- src/skyss0fly/welcomemessage/Main.php
+++ src/skyss0fly/welcomemessage/Main.php
@@ -8,23 +8,12 @@ use pocketmine\event\player\PlayerJoinEvent;
 use joejoe77777\FormAPI;
 
 class Main extends PluginBase implements Listener {
-const CONFIG_VERSION = 1;
     public function onEnable(): void {
         $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-  private function updateConfig(){
-        if (!file_exists($this->getDataFolder() . 'config.yml')) {
-            $this->saveResource('config.yml');
-            return;
-        }
-        if ($this->getConfig()->get('config-version') !== self::CONFIG_VERSION) {
-            $config_version = $this->getConfig()->get('config-version');
-            $this->getLogger()->info("Your Config isn't the latest. We renamed your old config to §bconfig-" . $config_version . ".yml §6and created a new config");
-            rename($this->getDataFolder() . 'config.yml', $this->getDataFolder() . 'config-' . $config_version . '.yml');
-            $this->saveResource('config.yml');
-        }
-  }
+
+ 
     public function onPlayerJoin(PlayerJoinEvent $event) {
         $player = $event->getPlayer();
         $servername = $this->getConfig()->get("ServerName");
