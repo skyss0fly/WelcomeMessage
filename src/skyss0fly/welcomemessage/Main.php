a76827feb2e4d5360dcec900120e63167d3a1901
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-03 16:13:25 +1000

    Update Main.php

--- src/skyss0fly/welcomemessage/Main.php
+++ src/skyss0fly/welcomemessage/Main.php
@@ -24,7 +24,7 @@ class Main extends PluginBase implements Listener {
         $servername = str_replace("&", "§", $servername);
         $message = str_replace("&", "§", $message);
         $message = $prefix . ": " . $message . $servername;
-if get($broadcasttoserver) === true
+if $broadcasttoserver === true
         $this->getServer()->broadcastMessage($message);
 else 
     $this->getServer()->whisper($player, $message);
