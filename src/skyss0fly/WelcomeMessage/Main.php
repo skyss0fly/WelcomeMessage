cf2d8129b082fe5ea0c52a62f75daead1317b086
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-12-15 19:24:13 +1000

    Rename ClassName and Implement 1.10 features

--- src/skyss0fly/WelcomeMessage/Main.php
+++ src/skyss0fly/WelcomeMessage/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace skyss0fly\welcomemessage;
+namespace skyss0fly\WelcomeMessage;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
@@ -10,42 +10,47 @@ use pocketmine\player\Player;
 
 class Main extends PluginBase implements Listener {
 
+    private $config:
     
     public function onEnable(): void {
+        $this->config() = $this->getServer()->getConfig();
         $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-
  
     public function onPlayerJoin(PlayerJoinEvent $event) {
        $player = $event->getPlayer();
-        
-
-         
+       if ($config->get("UseFormInsteadOfChat") === true {     
         $joinform = new SimpleForm(function (Player $player, $data){
             $result = $data;
-            if ($result !== null) {
-        
+            if ($result !== null) {  
                 switch ($result) {
-                    case 0:
-                  
+                    case 0:          
                     $submitmsg = $this->getConfig()->get("Submit-Msg");
-                    $player->sendMessage($submitmsg);
-                      
+                    $player->sendMessage($submitmsg);                   
                     break;
                 }
             }
         });
         $formtitle = $this->getConfig()->get("Form-Title");
-        $formcontent = $this->getConfig()->get("Form-Content");
-    
-       
+        $formcontent = $this->getConfig()->get("Form-Content");   
         $joinform->setTitle($formtitle);
         $joinform->setContent($formcontent);
         $joinform->addButton("§d§lSubmit!");
         $player->sendForm($joinform);
     }
-    
+} else {
+    $rawmessage = $this->config()->get("JoinMessage");
+    if ($rawmessage->str_contains("{player}")) {
+$message = str_replace("{player}", $player, $rawmessage);
+$this->getServer()->broadcastMessage($message)
+    }    
+    else {
+$message = $this->config()->get("JoinMessage");
+$this->getServer()->broadcastMessage($message);
+}
+}
+
   public function onPlayerLeave(PlayerQuitEvent $event) {
       $leavemsg = "";
        $player = $event->getPlayer();
