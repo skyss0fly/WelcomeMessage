e1b8c87ccd38d6441f9285224060d87bf3192909
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-12-15 19:39:57 +1000

    Update Main.php

--- src/skyss0fly/WelcomeMessage/Main.php
+++ src/skyss0fly/WelcomeMessage/Main.php
@@ -5,72 +5,56 @@ namespace skyss0fly\WelcomeMessage;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\{PlayerJoinEvent, PlayerQuitEvent};
-use skyss0fly\WelcomeMessage\Form\{Form, SimpleForm};
+use skyss0fly\welcomemessage\Form\{Form, SimpleForm};
 use pocketmine\player\Player;
 
 class Main extends PluginBase implements Listener {
 
-    private $config;
-    
     public function onEnable(): void {
-        $this->config() = $this->getServer()->getConfig();
         $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
- 
-    public function onPlayerJoin(PlayerJoinEvent $event) {
-       $player = $event->getPlayer();
-       if ($config->get("UseFormInsteadOfChat") === true {     
-        $joinform = new SimpleForm(function (Player $player, $data){
-            $result = $data;
-            if ($result !== null) {  
-                switch ($result) {
-                    case 0:          
-                    $submitmsg = $this->getConfig()->get("Submit-Msg");
-                    $player->sendMessage($submitmsg);                   
-                    break;
+
+    public function onPlayerJoin(PlayerJoinEvent $event): void {
+        $player = $event->getPlayer();
+        $config = $this->getConfig();
+
+        if ($config->get("UseFormInsteadOfChat") === true) {
+            $joinForm = new SimpleForm(function (Player $player, $data) {
+                if ($data !== null) {
+                    $submitMessage = $this->getConfig()->get("Submit-Msg");
+                    $player->sendMessage($submitMessage);
                 }
-            }
-        });
-        
-        $formtitle = $this->getConfig()->get("Form-Title");
-        $formcontent = $this->getConfig()->get("Form-Content");   
-        $joinform->setTitle($formtitle);
-        $joinform->setContent($formcontent);
-        $joinform->addButton("§d§lSubmit!");
-        $player->sendForm($joinform);  
-} else {
-    $rawmessage = $this->config()->get("JoinMessage");
-    if ($rawmessage->str_contains("{player}")) {
-$message = str_replace("{player}", $player, $rawmessage);
-$this->getServer()->broadcastMessage($message)
-    }    
-    else {
-$message = $this->config()->get("JoinMessage");
-$this->getServer()->broadcastMessage($message);
-}
-}
+            });
 
-  public function onPlayerLeave(PlayerQuitEvent $event) {
-      $leavemsg = "";
-       $player = $event->getPlayer();
-      $msg = $this->getConfig()->get("LeaveMessage");
-            $deactivate = $this->getConfig()->get("LeaveMessageEnabled");
-      if ($deactivate == "true") {
-if ($msg->str_contains("{player}")) {
+            $formTitle = $config->get("Form-Title");
+            $formContent = $config->get("Form-Content");
 
-    $leavemsg = str_replace("{player}" , $player->getName(), $msg);
-}
-
-    $this->getServer()->broadcastMessage($leavemsg);
+            $joinForm->setTitle($formTitle);
+            $joinForm->setContent($formContent);
+            $joinForm->addButton("§d§lSubmit!");
+            $player->sendForm($joinForm);
+        } else {
+            $rawMessage = $config->get("JoinMessage");
+            $message = str_replace("{player}", $player->getName(), $rawMessage);
+            $this->getServer()->broadcastMessage($message);
+        }
     }
-      elseif ($deactivate == "false") {
-          # Nothing
-      }
-      else {
-$this->getLogger()->error("An Error Has Occurred within the configuration of This Plugin, 'LeaveMessageEnabled has an invalid bool Value' To protect your server from corruption, this Plugin Will Disable itself ");
-          $this->getServer()->getPluginManager()->disablePlugin($this);
-      }
-}
 
+    public function onPlayerLeave(PlayerQuitEvent $event): void {
+        $player = $event->getPlayer();
+        $config = $this->getConfig();
+        $leaveMessageEnabled = $config->get("LeaveMessageEnabled");
+
+        if ($leaveMessageEnabled === true) {
+            $rawMessage = $config->get("LeaveMessage");
+            $message = str_replace("{player}", $player->getName(), $rawMessage);
+            $this->getServer()->broadcastMessage($message);
+        } elseif ($leaveMessageEnabled === false) {
+            // No action required
+        } else {
+            $this->getLogger()->error("Invalid value for 'LeaveMessageEnabled'. Disabling plugin to protect the server.");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+        }
+    }
 }
